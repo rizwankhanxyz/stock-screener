d849ccef8fd5b743c33de92bfb947c730bbaf30e
iconrizwankhan <rizwankhan.1992@gmail.com>
2024-11-04 22:44:49 +0530

    some changes in compliance report

--- sss/client/src/components/ComplianceReport.js
+++ sss/client/src/components/ComplianceReport.js
@@ -5,13 +5,15 @@ function ComplianceReport({ stock, onClose }) {
   return (
     <div className="compliance-report-modal">
       <div className="compliance-report-content">
-        <button className="close-button" onClick={onClose}>×</button>
-        <h2>Compliance Report</h2>
-        <h3>{stock.companyName}</h3>
-        <p><strong>Symbol:</strong> {stock.nseorbseSymbol}</p>
-        <p><strong>Exchange:</strong> {stock.exchange}</p>
-        <p><strong>Debts/Market Cap:</strong> {stock.debtsMarketCap}</p>
-        <p><strong>Compliance Status:</strong> {stock.financialScreeningStatus}</p>
+        <div className="compliance-header">
+                  <button className="close-button" onClick={onClose}><i className="bi bi-x"></i></button>
+        <center><h2>Compliance Report</h2></center>
+        </div>
+        <h3 style={{color: "#84BC62"}}>{stock.companyName}</h3>
+        <p style={{color: "#84BC62"}}><strong>Symbol:</strong> {stock.nseorbseSymbol}</p>
+        <p style={{color: "#84BC62"}}><strong>Exchange:</strong> {stock.exchange}</p>
+        <p style={{color: "#84BC62"}}><strong>Debts/Market Cap:</strong> {stock.debtsMarketCap}</p>
+        <p style={{color: "#84BC62"}}><strong>Compliance Status:</strong> {stock.financialScreeningStatus}</p>
         {/* Add other relevant stock information here */}
       </div>
     </div>
