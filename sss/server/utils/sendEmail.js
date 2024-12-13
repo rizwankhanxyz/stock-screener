c0a7f5cbbd9df7abe03a5c4ac7280a9d9d68f33a
iconrizwankhan <rizwankhan.1992@gmail.com>
2024-12-13 23:44:36 +0530

    sendEmail

--- sss/server/utils/sendEmail.js
+++ sss/server/utils/sendEmail.js
@@ -1,1 +1,19 @@
 //this is for Email
+
+import { Resend } from "resend";
+
+const resend = new Resend("re_Sh23aeV9_9cZ2nrYGPqhzgvBYoUnjcwoJ"); //change
+
+async function sendEmail(emailData) {
+  const { data, error } = await resend.emails.send({
+    from: "rizwan@rizwankhan.xyz",
+    to: emailData.to,
+    subject: emailData.subject,
+    html: emailData.body,
+  });
+  if (error) {
+    return console.error({ error });
+  }
+}
+
+export default sendEmail;
